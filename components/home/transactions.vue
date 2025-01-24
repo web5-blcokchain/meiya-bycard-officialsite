0ac0fe621d21640b7141b648651c78fc38891ba5
imba97 <mail@imba97.cn>
2025-01-24 20:09:22 +0800

    feat: transactions component

--- components/home/transactions.vue
+++ components/home/transactions.vue
@@ -1,18 +1,73 @@
 <template>
-  <div flex="~ lt-md:col-reverse" mx-a max-w-7xl w-full gap-10 px-4>
-    <div space-y-4>
-      <div text="11 lt-md:6 lt-md:center" font-semibold>
-        Redefining Online Transactions
+  <div flex="~ col" gap-10>
+    <div flex="~ lt-md:col-reverse" mx-a max-w-5xl w-full gap-10 px-4>
+      <div space-y-4>
+        <div text="11 lt-md:6 center" font-semibold>
+          Redefining Online Transactions
+        </div>
+        <div text="5 lt-md:4 center #666">
+          Discover the unparalleled features that make ByCard the go-to platform for secure,
+          convenient, and efficient online transactions. With our virtual credit cards,
+          you can seamlessly shop, subscribe, and transact online,
+          all with just a few clicks.
+        </div>
       </div>
-      <div text="5 lt-md:4 lt-md:center #666">
-        Discover the unparalleled features that make ByCard the go-to platform for secure,
-        convenient, and efficient online transactions. With our virtual credit cards,
-        you can seamlessly shop, subscribe, and transact online,
-        all with just a few clicks.
+      <div hidden lt-md:block text="#666 center">
+        About US
       </div>
     </div>
-    <div hidden lt-md:block text="#666 center">
-      About US
+
+    <div flex="~ lt-md:col" gap="7.5" mx-a max-w-7xl w-full items-center px-4>
+      <div p="7.5" h-85 w-70 bg-primary space-y-4>
+        <div>
+          <img src="~/assets/icons/credit-card.svg" size-11>
+        </div>
+        <div text="6 white">
+          Virtual Credit Cards
+        </div>
+        <div text-white>
+          Say goodbye to the limitations of physical credit cards.
+        </div>
+      </div>
+
+      <div h-85 w-70 space-y-4 p="7.5" b="2 solid black">
+        <div>
+          <img src="~/assets/icons/security.svg" size-11>
+        </div>
+        <div text-6>
+          Cutting-Edge Security
+        </div>
+        <div text="#666">
+          We employ state-of-the-art encryption and security protocols
+          to ensure your financial data is protected at all times.
+        </div>
+      </div>
+
+      <div h-85 w-70 space-y-4 p="7.5" b="2 solid black">
+        <div>
+          <img src="~/assets/icons/coin.svg" size-11>
+        </div>
+        <div text-6>
+          Transparent Fee Structure
+        </div>
+        <div text="#666">
+          It maintains a straightforward fee structure,
+          ensuring you always know what to expect, with no surprises.
+        </div>
+      </div>
+
+      <div h-85 w-70 space-y-4 p="7.5" b="2 solid black">
+        <div>
+          <img src="~/assets/icons/security.svg" size-11>
+        </div>
+        <div text-6>
+          User-Friendly Interface
+        </div>
+        <div text="#666">
+          Our platform boasts an intuitive, user-friendly interface,
+          designed for both beginners and experienced users.
+        </div>
+      </div>
     </div>
   </div>
 </template>
