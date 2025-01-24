e90973aa71095d44b27cea9c0031e93e67da282c
imba97 <mail@imba97.cn>
2025-01-24 18:32:26 +0800

    feat: transactions component

---
+++ components/home/transactions.vue
@@ -0,0 +1,18 @@
+<template>
+  <div flex="~ lt-md:col-reverse" mx-a max-w-7xl w-full gap-10 px-4>
+    <div space-y-4>
+      <div text="11 lt-md:6 lt-md:center" font-semibold>
+        Redefining Online Transactions
+      </div>
+      <div text="5 lt-md:4 lt-md:center #666">
+        Discover the unparalleled features that make ByCard the go-to platform for secure,
+        convenient, and efficient online transactions. With our virtual credit cards,
+        you can seamlessly shop, subscribe, and transact online,
+        all with just a few clicks.
+      </div>
+    </div>
+    <div hidden lt-md:block text="#666 center">
+      About US
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
   <div ma-a w-full space-y="40 lt-md:20">
     <HomeBanner />
     <HomeAbout />
+    <HomeTransactions />
   </div>
 </template>
