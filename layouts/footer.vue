13031a39d535838a5b889595c2cff07186b93cbf
imba97 <mail@imba97.cn>
2025-01-26 13:55:56 +0800

    fix: logo not displaying correctly

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -3,7 +3,7 @@
     <div lt-md="flex flex-col" mx-a max-w-7xl w-full gap-5 px-4 md:fbc>
       <div space-y-4>
         <div>
-          <NuxtImg src="/logo.svg" h-10 />
+          <img src="/logo.svg" h-10>
         </div>
         <div w-120 lt-md:w-full text="#666">
           Thank you for choosing ByCard as your trusted partner for secure online transactions.
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,8 +12,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     '@unocss/nuxt',
     '@pinia/nuxt',
-    '@nuxtjs/color-mode',
-    '@nuxt/image'
+    '@nuxtjs/color-mode'
   ],
 
   css: [
