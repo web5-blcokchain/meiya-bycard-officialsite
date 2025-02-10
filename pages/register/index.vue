a25cfec80ea52b36bb7001d6ae92bd98cf14c45d
imba97 <mail@imba97.cn>
2025-02-10 16:10:08 +0800

    feat: register api

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -75,36 +75,40 @@
               </p>
             </div>
 
-            <AForm :model="form">
+            <AForm ref="organization-form" :model="form" :rules="rules">
               <AFormItem name="mail">
-                <AInput v-model="form.mail" placeholder="Email" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.mail" placeholder="Email" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
               <AFormItem name="password">
                 <FormPasswordInput v-model="form.password" placeholder="Password" />
               </AFormItem>
-              <AFormItem>
+              <AFormItem name="confirmPassword">
                 <FormPasswordInput v-model="confirmPassword" placeholder="Confirm password" />
               </AFormItem>
               <AFormItem name="mobile">
-                <AInput v-model="form.mobile" placeholder="Mobile" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.mobile" placeholder="Mobile" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
               <AFormItem name="cust_name">
-                <AInput v-model="form.cust_name" placeholder="Customer name" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.cust_name" placeholder="Customer name" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
               <AFormItem name="logo">
-                <FormUpload>Organization logo</FormUpload>
+                <FormUpload v-model="form.logo">
+                  Customer logo
+                </FormUpload>
               </AFormItem>
               <AFormItem name="business_licence_no">
-                <AInput v-model="form.business_licence_no" placeholder="Business licence no" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.business_licence_no" placeholder="Business licence no" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
-              <AFormItem>
-                <FormUpload>Business license</FormUpload>
+              <AFormItem name="business_licence_file">
+                <FormUpload v-model="form.business_licence_file">
+                  Business license
+                </FormUpload>
               </AFormItem>
               <AFormItem name="juridical_person">
-                <AInput v-model="form.juridical_person" placeholder="Juridical person" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.juridical_person" placeholder="Juridical person" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
-              <AFormItem>
-                <ASelect v-model="form.juridical_person_type" placeholder="Juridical person type" size="large">
+              <AFormItem name="juridical_person_type">
+                <ASelect v-model:value="form.juridical_person_type" placeholder="Juridical person type" size="large">
                   <ASelectOption value="10000">
                     ID card
                   </ASelectOption>
@@ -114,27 +118,27 @@
                 </ASelect>
               </AFormItem>
               <AFormItem name="juridical_person_no">
-                <AInput v-model="form.juridical_person_no" placeholder="Juridical person no" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.juridical_person_no" placeholder="Juridical person no" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
               <AFormItem name="contacts">
-                <AInput v-model="form.contacts" placeholder="Contacts" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.contacts" placeholder="Contacts" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
-              <AFormItem>
-                <ASelect placeholder="Select Country and Region" size="large">
+              <AFormItem name="country_code">
+                <ASelect v-model:value="form.country_code" placeholder="Select Country and Region" size="large" @change="selectCountry">
                   <ASelectOption v-for="item in countryCodes" :key="item.code" :value="item.code">
                     {{ item.code }} {{ item.name }}
                   </ASelectOption>
                 </ASelect>
               </AFormItem>
               <AFormItem name="contact_way">
-                <AInput v-model="form.contact_way" placeholder="Contact information of institution contact person" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.contact_way" placeholder="Contact information of institution contact person" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
               <AFormItem name="invitation_code_ref">
-                <AInput v-model="form.invitation_code_ref" placeholder="Invitation code" size="large" bg="#f3f4f6" b-none py-3 />
+                <AInput v-model:value="form.invitation_code_ref" placeholder="Invitation code" size="large" bg="#f3f4f6" b-none py-3 />
               </AFormItem>
 
               <AFormItem mt-4>
-                <AButton type="primary" size="large" block @click="router.push('/register/org-success')">
+                <AButton type="primary" size="large" :loading="loading" block @click="organizationRegister">
                   Register
                 </AButton>
               </AFormItem>
@@ -147,6 +151,10 @@
 </template>
 
 <script setup lang="ts">
+import type { Rule } from 'ant-design-vue/es/form'
+
+import { message } from 'ant-design-vue'
+
 definePageMeta({
   layout: 'login'
 })
@@ -154,6 +162,8 @@ definePageMeta({
 const router = useRouter()
 
 const countryCodes = useCountry()
+
+const loading = ref(false)
 
 const form = reactive({
   country_code: '',
@@ -177,4 +187,64 @@ const form = reactive({
 const confirmPassword = ref('')
 
 const agree = ref(false)
+
+const organizationForm = useTemplateRef<any>('organization-form')
+
+const rules: Record<string, Rule[]> = {
+  mail: [{ required: true, message: 'Please enter your email address', trigger: 'blur' }],
+  password: [{ required: true, message: 'Please enter your password', trigger: 'blur' }],
+  confirmPassword: [
+    {
+      validator: () => {
+        if (confirmPassword.value !== form.password) {
+          return Promise.reject(new Error('The two passwords do not match'))
+        }
+        return Promise.resolve()
+      },
+      trigger: 'blur'
+    }
+  ],
+  mobile: [{ required: true, message: 'Please enter your mobile number', trigger: 'blur' }],
+  logo: [{ required: true, message: 'Please upload the customer logo', trigger: 'blur' }],
+  business_licence_file: [{ required: true, message: 'Please upload the business license', trigger: 'blur' }],
+  country_code: [{ required: true, message: 'Please select the country and region', trigger: 'blur' }],
+  cust_name: [{ required: true, message: 'Please enter the customer name', trigger: 'blur' }],
+  business_licence_no: [{ required: true, message: 'Please enter the business license number', trigger: 'blur' }],
+  juridical_person_type: [{ required: true, message: 'Please select the type of legal person', trigger: 'blur' }],
+  juridical_person: [{ required: true, message: 'Please enter the legal person', trigger: 'blur' }],
+  juridical_person_no: [{ required: true, message: 'Please enter the legal person number', trigger: 'blur' }],
+  contacts: [{ required: true, message: 'Please enter the contact person', trigger: 'blur' }],
+  contact_way: [{ required: true, message: 'Please enter the contact information of the institution contact person', trigger: 'blur' }]
+}
+
+function selectCountry(value: string) {
+  const countryName = _find(countryCodes, { code: value })?.name
+
+  if (!countryName) {
+    message.error('Country code error')
+    return
+  }
+
+  form.country = countryName
+}
+
+async function organizationRegister() {
+  await organizationForm.value.validate()
+
+  loading.value = true
+
+  customersApi
+    .register(form)
+    .then((response) => {
+      if (response.data.code !== 0) {
+        message.error(response.data.msg)
+        return
+      }
+
+      router.push('/register/org-success')
+    })
+    .finally(() => {
+      loading.value = false
+    })
+}
 </script>
