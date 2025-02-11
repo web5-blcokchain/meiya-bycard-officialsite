d9178307b43641598adbae7919e01c1d33c4a02f
imba97 <mail@imba97.cn>
2025-02-11 13:40:09 +0800

    chore: rename to customer

--- composables/api/customer.ts
+++ composables/api/customer.ts
@@ -1,6 +1,9 @@
 const { axios } = useRequest()
 
 export const customersApi = {
+  login(data: Record<string, any>) {
+    return axios.post('/customers/login', data)
+  },
   register(data: Record<string, any>) {
     return axios.post('/customers/register', data)
   }
